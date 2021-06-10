e65129868dbd84e3a78173e7684c3631e8b68434
Joe <JoeWDimmick@gmail.com>
2021-06-09 18:26:51 -0700

    restructured the file structure

---
+++ Artifactory/src/test/java/com/AbundantCauldron/Artifactory/ArtifactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AbundantCauldron.Artifactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
